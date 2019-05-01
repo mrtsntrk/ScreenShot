19d6fb55e2781554f56a765bcb460dba01b95815
Kasım Mert ŞENTÜRK <ksm_mrtsntrk@outlook.com>
2019-05-01 20:07:55 +0300

    mertinkotr

---
+++ ekran_goruntusu/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace ekran_goruntusu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(12, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(356, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ekran Görüntüsü Çek!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 102);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KMS - Ekran Görüntüsü Programı";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ekran_goruntusu/Form2.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace ekran_goruntusu
+{
+    public partial class Form2 : Form
+    {
+        int selectX;
+        int selectY;
+        int selectWidth;
+        int selectHeight;
+        public Pen selectPen;
+
+        bool start = false;
+        private void SaveToClipboard()
+        {
+            if(selectWidth >0)
+            {
+                Rectangle rect = new Rectangle(selectX, selectY, selectWidth, selectHeight);
+                Bitmap OrginalImage = new Bitmap(pictureBox1.Image, pictureBox1.Width, pictureBox1.Height);
+                Bitmap _img = new Bitmap(selectWidth, selectHeight);
+                Graphics g = Graphics.FromImage(_img);
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+                g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                g.DrawImage(OrginalImage, 0, 0, rect, GraphicsUnit.Pixel);
+                Clipboard.SetImage(_img);
+            }
+            //End application
+            //Application.Exit();
+            Form1 frm1 = new Form1();
+            this.Close();
+            frm1.ShowDialog();
+
+        }
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphic = Graphics.FromImage(printscreen as Image);
+            graphic.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+            using (MemoryStream s = new MemoryStream())
+            {
+                printscreen.Save(s, ImageFormat.Bmp);
+                pictureBox1.Size = new System.Drawing.Size(this.Width, this.Height);
+                pictureBox1.Image = Image.FromStream(s);
+
+            }
+            this.Show();
+            Cursor = Cursors.Cross;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(pictureBox1.Image == null)
+                return;
+                 if (start)
+            {
+                pictureBox1.Refresh();
+                selectWidth = e.X - selectX;
+                selectHeight = e.Y - selectY;
+                pictureBox1.CreateGraphics().DrawRectangle(selectPen, selectX, selectY, selectWidth, selectHeight);
+
+            }
+            
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(!start)
+            {
+                if(e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    selectX = e.X;
+                    selectY = e.Y;
+                    selectPen = new Pen(Color.Red, 1);
+                    selectPen.DashStyle = DashStyle.DashDotDot;
+                }
+                pictureBox1.Refresh();
+                start = true;
+            }
+            else
+            {
+                if (pictureBox1.Image == null)
+                    return;
+                if(e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    pictureBox1.Refresh();
+                    selectWidth = e.X - selectX;
+                    selectHeight = e.Y - selectY;
+                    pictureBox1.CreateGraphics().DrawRectangle(selectPen, selectX, selectY, selectWidth, selectHeight);
+
+                }
+                start = false;
+                SaveToClipboard();
+            }
+        }
+    }
+}
